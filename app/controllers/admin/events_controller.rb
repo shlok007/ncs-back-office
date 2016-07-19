10a6658b5fb3c5c266e9e8a867a604068d513bdd
shlok007 <shlok.srivastava15@gmail.com>
2016-07-19 01:23:23 -0400

    Implemented #9: Ability to control events

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,4 +1,17 @@
 module Admin
   class EventsController < Admin::ApplicationController
+
+    #Only admins are allowed to create new events.
+    def new
+      if !current_user.has_role? :admin
+        redirect_to admin_events_path, alert: "You are not authorised to perform this action"
+      end
+    end
+
+    def destroy
+      if !current_user.has_role? :admin
+        redirect_to admin_events_path, alert: "You are not authorised to perform this action"
+      end
+    end
   end
 end
--- app/dashboards/event_dashboard.rb
+++ app/dashboards/event_dashboard.rb
@@ -1,13 +1,21 @@
 require "administrate/base_dashboard"
 
 class EventDashboard < Administrate::BaseDashboard
-	  # ATTRIBUTE_TYPES
+    # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
   # Each different type represents an Administrate::Field object,
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    name: Field::String,
+    start_time: Field::DateTime,
+    end_time: Field::DateTime,
+    display_start_time: Field::DateTime,
+    display_end_time: Field::DateTime,
+    description: Field::Text,
+    contact_info: Field::String,
+    venue: Field::Text,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -16,16 +24,17 @@ class EventDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :name,
+    :start_time,
+    :end_time,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = [
-  ].freeze
+  SHOW_PAGE_ATTRIBUTES = ATTRIBUTE_TYPES.keys
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = [
-  ].freeze
+  FORM_ATTRIBUTES = ATTRIBUTE_TYPES.keys
 end
