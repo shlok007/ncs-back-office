6cf6f787e1a98e3af042f61a24ff7349537617aa
shlok007 <shlok.srivastava15@gmail.com>
2016-08-04 22:23:41 -0400

    Modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,9 @@ users.each do |role|
 	User.create(name: "#{role}", email: "#{role}@hackncs.com", password: "12345678", password_confirmation: "12345678", club: "Programming", linkedin: "http://linked.com/in/user_#{role}", github: "http://github.com/user_#{role}", admission_no: "15/CS/#{random.rand(1..140)}")
 end
 
+User.first.add_role :admin
+User.second.add_role :editor
+
 (1..10).each do |id|
 	Student.create(name: "Student ##{id}", email: "Student_#{id}@hackncs.com", year: random.rand(1..4), contact_no: 9717798598, admission_no: "15/CS/#{random.rand(1..120)}", )
 
